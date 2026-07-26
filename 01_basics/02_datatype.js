6460dde1cf8bba1d78c5a57b1cc7c9af60e00c93
Aditya Bhamare <adityabhamare@Adityas-MacBook-Air.local>
2026-07-26 23:52:33 +0530

    learnt about various datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as never version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("aadi")
+
+let name = "aditya"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // null is object not an type (in js).
+console.log(typeof undefined);// undefined type is undefined.
+console.log(typeof "aditya"); // name type is string.
+// age type is number.

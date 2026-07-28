410be61169dd35bb2a42049f88da6b125f0effab
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-07-29 00:16:53 +0530

    conversion and operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33" // if 33 is written as "33" then the output will be string not the number
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 // "33abc" => NaN
 // true => 1; false => 0
@@ -21,5 +21,36 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********** Operation ************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
